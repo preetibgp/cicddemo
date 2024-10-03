ece09a3f8ccbaeb814386ce7c1c85f4c2432a798
Preeti <Preeti.p09@squareyards.co.in>
2024-10-03 11:20:35 +0530

    minor changes in main file.

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({ Key ? key}) : super(key: key);
 
-  
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
